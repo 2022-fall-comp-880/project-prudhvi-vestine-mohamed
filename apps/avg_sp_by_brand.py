b67ec282f8fb37af7f211c345b69226555e7e182
elbakri73 <me1116@unh.edu>
2022-12-07 15:38:47 -0500

    Complete the code the for avg_sp_by_brand.py

--- apps/avg_sp_by_brand.py
+++ apps/avg_sp_by_brand.py
@@ -13,15 +13,16 @@ class BrandPrice:
     Attributes:
         filename: string
     """
-    def init(self, filename):
+
+    def __init__(self, filename):
         """
         Instance variable initialization
         :param filename: string
         """
         self.filename = filename
 
-    def file_to_dictionary(self, key_column: int, value_column: int) -> dict
-        [str, list[int]]:
+    def file_to_dictionary(self, key_column: int, value_column: int) -> dict[
+        str, list[int]]:
         """
         Reads from text file named self. filename using csv.reader() method
         and creates a dictionary with the data in column key_column as keys and
@@ -34,8 +35,18 @@ class BrandPrice:
             key: string, representing brand in column key_file
             value: List of int values corresponding values in column value_column
         """
-
-
+        car_dict = {}
+        with open(self.filename, encoding='utf-8', newline='') as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            for count, row in enumerate(reader):
+                if count != 0:
+                    key = row[key_column]
+                    value = int(row[value_column])
+                    if key in car_dict:
+                        car_dict[key].append(value)
+                    else:
+                        car_dict[key] = [value]
+        return car_dict
 
     def brand_by_price_d(self):
         """
@@ -45,7 +56,10 @@ class BrandPrice:
             key: str, representing brand name
             value: list of integers, representing selling prices of cars
         """
-
+        brand_idx = 2
+        price_idx = 1
+        brand_by_price = self.file_to_dictionary(brand_idx, price_idx)
+        return brand_by_price
 
     def average_selling_price_by_brand(self) -> dict[str, float]:
         """
@@ -57,5 +71,14 @@ class BrandPrice:
            key: str, Brand
            value: Float, Average selling price of corresponding brand car.
         """
+        brand_dict = self.brand_by_price_d()
+        brand_avg_sp_dict = {}
+        for brand, prices_list in brand_dict.items():
+            sum_prices = 0
+            count = 0
+            for price in prices_list:
+                sum_prices += price
+                count += 1
+            brand_avg_sp_dict[brand] = round(sum_prices / count, 2)
 
-
+        return brand_avg_sp_dict
